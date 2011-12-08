51b6be98c33f48b70d9b7dad956704c05a63f801
Joshua Carver <=>
2011-12-08 13:39:40 -0800

    add cname

--- create_post.rb
+++ create_post.rb
@@ -4,5 +4,14 @@ require 'fileutils'
 
 raise "Usage: ./create_post.rb <post_title>" if ARGV.length < 1 
 post_title = ARGV[0]
-FileUtils.touch "_posts/#{Time.now.strftime('%Y-%m-%d')}-#{post_title}.textile"
+filename  = "#{post_title.gsub(' ','-').gsub(/[^a-zA-Z0-9\-]/, '')}.textile"
+
+File.open "_posts/#{Time.now.strftime('%Y-%m-%d')}-#{filename}", 'w' do |file|
+  file.puts <<-TEXT
+---
+layout: post
+title: #{post_title} 
+---
+  TEXT
+end
 
